c0868033902cf02c0e0c4441faf83c6ef1445dad
asma <asmaammar29@gmail.com>
2023-08-11 14:25:12 -0700

    its commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	int digit = 0, digit2;
+	int digit1 = 0, digit2;
 
 	while (digit1 <= 9)
 	{
@@ -30,7 +30,7 @@ int main(void)
 			}
 			digit2++;
 		}
-		digit1++
+		digit1++;
 	}
 	putchar('\n');
 
