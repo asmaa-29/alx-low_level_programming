635796eede744fd2653bbf1db02edea65b412b7c
asma <asmaammar29@gmail.com>
2023-08-11 13:01:02 -0700

    its commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Descroption: print 0-9 using putchar
+ *              while using int variable
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
