3bb59097667aad223618ac3e1a0ff850d36a5811
asma <asmaammar29@gmail.com>
2023-08-24 16:56:01 -0700

    its my first commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,21 +10,27 @@
 
 char *cap_string(char *s)
 {
-	char *ptr = s;
-	int foundDelimit = 1;
+	int count;
 
-	while (*s)
+	count = 0;
+	while (s[count] != '\0')
 	{
-		if (isDelimiter(*s))
-			foundDelimit = 1;
-		else if (islower(*s) && foundDlimit)
+		if (s[0] >= 97 && s[0] <= 122)
 		{
-			*s -= 32;
-			foundDelimit = 0;
+			s[0] = s[0] - 32;
 		}
-		else
-			foundDelimit = 0;
-		s++;
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
 	}
-	return (ptr);
+	return (s);
 }
