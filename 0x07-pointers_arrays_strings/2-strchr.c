1674c63068f27b349f023f4873cee6b6d71174f9
asma <asmaammar29@gmail.com>
2023-08-28 10:51:33 -0700

    its my first commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - fills the memory with constant byte
+ *
+ * @s: pointer to put the constant
+ * @c: constant
+ *
+ * Return: pointer s
+ */
+char *_strchr(char *s, char c);
+{
+	int i = 0;
+
+	for (s[i] != '\0' && s[i] != c)
+	{
+		i++;
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+		return (NULL);
+}
