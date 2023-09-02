f66601fa51a13e82729c54d5a7cbfa30d5ef840f
asma <asmaammar29@gmail.com>
2023-09-01 17:34:20 -0700

    its my first commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
 * @argv: list
 * Return: 0 on success
 */
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	printf("%s\n", argv[0]);
 	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
