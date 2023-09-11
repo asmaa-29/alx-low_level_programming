14560b4ba9f02e8539f434b142784069c4e62e3c
asma <asmaammar29@gmail.com>
2023-09-11 06:00:35 -0700

    its my first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,18 +2,19 @@
 #define DOG_H
 
 /**
-*struct dog - a new type of data
-*representing a dog
-*@name: name of the dog
-*@age: age of the dog
-*@owner: owner's name
+ * struct dog - a dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner's name
 */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+typedef struct dog dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
