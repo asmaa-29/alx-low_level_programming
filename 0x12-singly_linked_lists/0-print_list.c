b16ac50993f932abbc55e793ec3b715658c2d92b
asma <asmaammar29@gmail.com>
2023-09-21 11:13:02 -0700

    itsmy first commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,16 @@ size_t print_list(const list_t *h)
 {
 
 
-	register int i = 0;
+	register int len = 0;
 
 	while (h)
 	{
 		(!h->str)
 			? printf("[0] (nil)\n")
-			: printf("[%i] %s\n", h->i, h->str);
+			: printf("[%i] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
-	return (i);
+	return (len);
 
 }
